e299b420465592e4c7d60127e70344ee7ee8e08c
mallasampat <mallasampat@gmail.com>
2025-07-31 19:03:55 +0530

    Fix

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: process.env.VITE_BASE_PATH || "/survey-form-demo",
+  // base: process.env.VITE_BASE_PATH || "/survey-form-demo",
   // server: {
   //   allowedHosts: ["cd159b8322ff.ngrok-free.app"],
   // },
